ae7e70a499156c7a5647bca92fcbd6cef189104b
morris <406108957morris@gmail.com>
2021-03-11 17:13:59 +0800

    homework customer

--- src/test/java/com/morris/Customer.java
+++ src/test/java/com/morris/Customer.java
@@ -3,22 +3,22 @@ package com.morris;
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class Customer {
-    String 客戶編號;
-    int 消費金額;
-    int 折讓;
-    int 金額;
+    String id;
+    int spent;
+    int discount;
 
-    public Customer(String 客戶編號, int 消費金額, int 折讓, int 金額) {
-        this.客戶編號 = 客戶編號;
-        this.折讓 = 折讓;
-        this.消費金額 = 消費金額;
-        this.金額 = 金額;
+
+    public Customer(String id, int spent, int discount) {
+        this.id = id;
+        this.discount = discount;
+        this.spent = spent;
+
 
 
     }
 
     public void print() {
-        System.out.println(客戶編號+ "\t" + 消費金額 + "\t"+折讓 + "\t" + 金額 );
+        System.out.println(id+ "\t" + spent + " \t"+ discount  +"\t" +"  "+ (spent-discount));
 
 
     }
--- src/test/java/com/morris/Homeworkcustomer.java
+++ src/test/java/com/morris/Homeworkcustomer.java
@@ -1,10 +1,18 @@
 package com.morris;
 
 public class Homeworkcustomer {
-    Customer [] customers={
-            new Customer("0013 ",490,0,490),
-            new Customer("0052 ",1000,100,900),
-            new Customer("0081 ",290,0,290),
-            new Customer("2122 ",2000,200,1800),
-    };
+    public static void main(String[] args) {
+
+
+        Customer[] customers = {
+                new Customer("0013 ", 490, 0),
+                new Customer("0052 ", 1000, 100),
+                new Customer("0081 ", 290, 0),
+                new Customer("2122 ", 2000, 200),
+        };
+        for (int i = 0; i <customers.length ; i++) {
+            customers[i].print();
+        }
+    }
+
 }
